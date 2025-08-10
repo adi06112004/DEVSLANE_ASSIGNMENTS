a8919d793971888890117a2f1e3e20c9b7f64b49
Aditya Prasad <adityak2541@gmail.com>
2025-08-10 10:27:02 +0530

    Update CartRow.jsx

--- Assignment_71.1/src/components/CartRow.jsx
+++ Assignment_71.1/src/components/CartRow.jsx
@@ -21,9 +21,9 @@ const CartRow = ({handleChange,handleRemove,cart,product}) => {
       <button onClick={(e)=>{onHandleRemove(+e.target.value)}} className='w-6 hover:cursor-pointer'><RxCross2 /></button>
       <img className='w-[50px] h-[50px]' src={product.thumbnail} alt='img' />
       <h1 className='w-1/3 text-orange-500 font-bold'>{product.title}</h1>
-      <p className='w-[50px] text-sm font-bold'>{product.price}</p>
+      <p className='w-[50px] text-sm font-bold'>₹{product.price}</p>
       <input onChange={onHandleChange} className='w-[50px] border border-gray-300 pl-1' type="number" value={noProduct} />
-      <p className='w-[50px] text-sm font-bold'>{(cart[product.id] * product.price).toFixed(2)}</p>
+      <p className='w-[50px] text-sm font-bold'>₹{(cart[product.id] * product.price).toFixed(2)}</p>
     </div>
   );
 };
