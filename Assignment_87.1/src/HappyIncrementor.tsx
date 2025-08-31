6eceadb652c5dbb00d8b4b9f91e1598227e41684
Aditya Prasad <adityak2541@gmail.com>
2025-08-31 15:35:04 +0530

    Updated Code

--- Assignment_87.1/src/HappyIncrementor.tsx
+++ Assignment_87.1/src/HappyIncrementor.tsx
@@ -1,25 +1,29 @@
-import { FC, memo } from "react";
+import type { FC } from "react";
+import { memo } from "react";
 import { useDispatch } from "react-redux";
 import { happyButtonClicked } from "./action";
 
 type HappyIncrementorProps = {};
 
-const HappyIncrementor: FC<HappyIncrementorProps> = (props) => {
+const HappyIncrementor: FC<HappyIncrementorProps> = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  function increment() {
+    // Dispatch the action correctly
+    dispatch(happyButtonClicked);
+  }
 
-    function increment(){
-        dispatch(happyButtonClicked);
-    }
-
-    return (
-        <div>
-            <h3>Are you happy?</h3>
-            <button onClick={increment} className="bg-orange-700 hover:cursor-pointer px-3 py-1 rounded">Yes</button>
-        </div>
-    );
-}
-
-HappyIncrementor.defaultProps = {};
+  return (
+    <div>
+      <h3>Are you happy?</h3>
+      <button
+        onClick={increment}
+        className="bg-orange-700 hover:cursor-pointer px-3 py-1 rounded"
+      >
+        Yes
+      </button>
+    </div>
+  );
+};
 
 export default memo(HappyIncrementor);
--- Assignment_87.1/src/SadTracker.tsx
+++ Assignment_87.1/src/SadTracker.tsx
@@ -1,20 +1,18 @@
-import { FC, memo } from "react";
+import type { FC } from "react";
+import { memo } from "react";
 import { useSelector } from "react-redux";
 import { sadCountSelector } from "./selectors";
 
 type SadTrackerProps = {};
 
-const SadTracker: FC<SadTrackerProps> = (props) => {
+const SadTracker: FC<SadTrackerProps> = () => {
+  const sadCount = useSelector(sadCountSelector);
 
-    const sadCount = useSelector(sadCountSelector);
-
-    return (
-        <div className="bg-blue-700 px-8 py-4">
-            You were Sad {sadCount} times
-        </div>
-    );
-}
-
-SadTracker.defaultProps = {};
+  return (
+    <div className="bg-blue-700 px-8 py-4">
+      You were sad {sadCount} times
+    </div>
+  );
+};
 
 export default memo(SadTracker);
